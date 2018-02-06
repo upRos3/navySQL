34306e52adfc7f492572cdc8dd4074cb1465ca9a
J Rose <josh@upros3.com>
2018-02-06 21:36:34 +0000

    first commit

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,12 +1,14 @@
+DROP SCHEMA navy;
+DROP TABLE ship;
+DROP TABLE duty;
+DROP TABLE shipduty;
+DROP TABLE fleet;
+DROP TABLE sailor;
+
 CREATE SCHEMA navy;
 
 CREATE TABLE fleet (
   fleetname TEXT PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE ship (
-  ship TEXT PRIMARY KEY NOT NULL,
-  datebuilt DATE NOT NULL
 );
 
 CREATE TABLE sailor (
@@ -15,15 +17,22 @@ CREATE TABLE sailor (
   dateofbirth DATE NOT NULL
 );
 
+CREATE TABLE ship (
+  shipname TEXT PRIMARY KEY NOT NULL,
+  datebuilt DATE NOT NULL,
+  fleetname TEXT REFERENCES fleet
+);
+
 CREATE TABLE duty (
   dutyid INT PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
-  end_date DATE NOT NULL
+  end_date DATE NOT NULL,
+  navyid INT REFERENCES sailor
 );
 
 CREATE TABLE shipduty (
-  dutyid INT,
-  shipname TEXT,
+  dutyid INT REFERENCES duty,
+  shipname TEXT REFERENCES ship,
   rank TEXT
 );
 
