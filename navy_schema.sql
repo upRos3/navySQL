6884bda5bc1bec4cd39f6d33a80b034ffc749a3f
J Rose <josh@upros3.com>
2018-02-06 17:59:27 +0000

    Created code to create navy db

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,30 @@
-SELECT 'hello, PG';
+CREATE SCHEMA navy;
+
+CREATE TABLE fleet (
+  fleetname TEXT PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE ship (
+  ship TEXT PRIMARY KEY NOT NULL,
+  datebuilt DATE NOT NULL
+);
+
+CREATE TABLE sailor (
+  navyid INT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  dateofbirth DATE NOT NULL
+);
+
+CREATE TABLE duty (
+  dutyid INT PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
+);
+
+CREATE TABLE shipduty (
+  dutyid INT,
+  shipname TEXT,
+  rank TEXT
+);
+
+SELECT * FROM fleet;
