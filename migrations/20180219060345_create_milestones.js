ee8762c6c68ae0b9a6740966dfce967fd97691c3
J Rose <josh@upros3.com>
2018-02-19 06:19:08 +0000

    finished migrations exercise

--- migrations/20180219060345_create_milestones.js
+++ migrations/20180219060345_create_milestones.js
@@ -1,16 +1,13 @@
-
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('milestones', function(table){
-      table.increments('id');
-      table.string('description');
-      table.date('date_achieved');
+    knex.schema.createTable("milestones", function(table) {
+      table.increments("id");
+      table.string("description");
+      table.date("date_achieved");
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-   return Promise.all([
-    knex.schema.dropTable('milestones')
-  ]);
+  return Promise.all([knex.schema.dropTable("milestones")]);
 };
